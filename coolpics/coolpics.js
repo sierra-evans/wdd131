5569c6e34bbe43bbc5b62cb34245bde2a3cfd7b3
Sierra Evans <eva22011@byui.edu>
2025-01-30 13:16:22 -0700

    Progress Prove 04

---
+++ coolpics/coolpics.js
@@ -0,0 +1,50 @@
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+  menu.classList.toggle("show");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("show");
+        menu.classList.add("large");
+    }
+    else {
+        menu.classList.remove("show");
+        menu.classList.remove("large");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+    const clickedElement = event.target;
+	// get the src attribute from that element and 'split' it on the "-"
+    const imgSrc = clickedElement.getAttribute("src").split("-")
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const newImgSrc = `${imgSrc[0]}-full.jpeg`;
+	// insert the viewerTemplate into the top of the body element
+	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newImgSrc));
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
